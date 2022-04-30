22bca2b3c80f724eb39f687e0e76d9f30fffa614
Michael Franco <franco.luis.michael@gmail.com>
2022-04-30 18:17:38 -0500

    commit tree d923fb6caebbda52ec7d6d022ef9d96bd21df1a2

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,29 @@
 class Dog
+  # setter method
+  def name=(name)
+    @name = name
+  end
+
+  # getter method
+  def name
+    @name
+  end
+
+    # setter method
+    def breed=(breed)
+        @breed = breed
+      end
+    
+      # getter method
+      def breed
+        @breed
+      end
+fido = Dog.new
+fido.name="Fido"
+
+snoopy = Dog.new
+snoopy.name="Snoopy"
+snoopy.breed="Beagle"
+    
 
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,26 @@
 class Person
-
+    def name=(name)
+        @name = name
+      end
+    
+      # getter method
+      def name
+        @name
+      end
+    
+        # setter method
+        def job=(job)
+            @job = job
+          end
+        
+          # getter method
+          def job
+            @job
+          end
+    beyonce = Person.new
+    beyonce.name= "Beyonce"
+    beyonce.job= "Singer"
+        
+   
+        
 end
